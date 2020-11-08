99e301a9e0d2aac0e24c7477d5eb87ae0b414eae
payamrastogi <payamrastogi@outlook.com>
2020-11-08 01:55:59 -0500

    add surveillance.py

--- surveillance.py
+++ surveillance.py
@@ -2,6 +2,9 @@
 from datetime import datetime, time
 from picamera import PiCamera, Color
 from time import sleep
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import glob, os
 
 def getCameraInstance():
     camera = PiCamera()
@@ -12,19 +15,42 @@ def getCameraInstance():
 
 def capture(camera):
     camera.start_preview()
-    for i in range(1):
-        sleep(30)
+    while(True):
         now = datetime.now()
         nowTime = now.time()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         camera.brightness = 50
+        sleepTime = 30
         if nowTime >= time(18,00) or nowTime <= time(7,00):
             camera.brightness = 55
+            sleepTime = 120
         camera.annotate_foreground = Color('yellow')
         camera.annotate_text = dt_string
         camera.capture('/home/pi/upload/picam-%s.jpg' %now)
+        upload(drive)
+        sleep(sleepTime)
     camera.stop_preview()
 
+
+def getDriveInstance():
+    gauth = GoogleAuth()
+    gauth.CommandLineAuth()
+    drive = GoogleDrive(gauth)
+    return drive
+
+def upload(drive):
+    os.chdir("/home/pi/upload")
+    for file in glob.glob("*.jpg"):
+        print (file)
+        with open(file, "r") as f:
+            fn = os.path.basename(f.name)
+            file_drive = drive.CreateFile({ 'title': fn })
+            file_drive.SetContentFile(fn)
+            file_drive.Upload()
+            print ("The file: " + fn + " has been uploaded ")
+            os.remove(fn)
+
 if __name__ == '__main__':
+    drive = getDriveInstance()
     camera = getCameraInstance()
     capture(camera)
