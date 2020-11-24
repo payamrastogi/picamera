d78e3d9cd18598fece79cdb3539916072f4c6ea3
payamrastogi <payamrastogi@outlook.com>
2020-11-23 22:31:35 -0500

    added motion sensor

---
+++ motionSense.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+from datetime import datetime, time
+from gpiozero import MotionSensor
+from picamera import PiCamera
+import logging
+import logging.config
+
+class MotionSense:
+    def __init__(self):
+        self.camera = PiCamera()
+        self.pirMotionSensor = MotionSensor(4)
+        logging.config.fileConfig('motionSenseLog.conf',disable_existing_loggers=0)
+        self.logger = logging.getLogger('motionSenseLogger')
+
+    def detect(self):
+        print("Called")
+        fileName = ''
+        self.pirMotionSensor.wait_for_motion()
+        self.logger.info("Motion Detected")
+        print("Motion Detected")
+        if not self.isDark():
+            self.camera.brightness = 50
+        else:
+            self.camera.brightness = 55
+
+        now = datetime.now()
+        dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+        self.camera.annotate_text = dt_string
+        fileName = '/home/pi/workspace/picamera/uploads/picam-{}.h264'.format(now)
+        self.camera.start_recording(fileName)
+        self.pirMotionSensor.wait_for_no_motion()
+        self.logger.info("Captured:"+fileName)
+        self.camera.stop_recording()
+        return fileName
+
+    def isDark(self):
+        now = datetime.now()
+        nowTime = now.time()
+        if nowTime >= time(18,00) or nowTime <= time(7,00):
+            return True
+        return False
+
+
+motionSense = MotionSense()
+while True:
+    motionSense.detect()
